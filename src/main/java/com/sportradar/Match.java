1ba1a65104d1d0c9dc5518c441dee56ab5421a19
INJUST1C <voportno@gmail.com>
2025-04-05 18:40:44 +0200

    Implement Match class

--- src/main/java/com/sportradar/Match.java
+++ src/main/java/com/sportradar/Match.java
@@ -1,4 +1,34 @@
 package com.sportradar;
 
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
 public class Match {
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private final Instant startTime;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.startTime = Instant.now();
+    }
+
+    public void updateScore(int home, int away) {
+        this.homeScore = home;
+        this.awayScore = away;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam + " " + homeScore + " - " + awayScore + " " + awayTeam;
+    }
 }
